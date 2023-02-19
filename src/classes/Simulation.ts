71855ef718aa105b521dbecceedfd97f33864571
Mardax <62964422+mardax007@users.noreply.github.com>
2023-02-19 16:43:53 +0100

    feat: :sparkles: scalability

--- src/classes/Simulation.ts
+++ src/classes/Simulation.ts
@@ -2,7 +2,7 @@ import { Coordinate, Tile, direction, tileType } from "../types.js";
 import { Road } from "./Road.js";
 import { directions } from "./utils.js";
 
-function generateTile(lastTile: Tile | null = null): [tileType, Coordinate] {
+function generateTile(lastTile: Tile | null = null, tileSize: number): [tileType, Coordinate] {
 	if (lastTile === null) {
 		return [{from: "top", to: "bottom"}, [0, 0]];
 	}
@@ -10,13 +10,13 @@ function generateTile(lastTile: Tile | null = null): [tileType, Coordinate] {
 	const from = directions[(directions.indexOf(lastTile.type.to) + 2) % 4];
 	const disallowedDirections: direction[] = [from]
 
-	if (lastTile.topLeft[0] <= 40) {
+	if (lastTile.topLeft[0] <= tileSize) {
 		disallowedDirections.push("left");
-	} if (lastTile.topLeft[1] <= 40) {
+	} if (lastTile.topLeft[1] <= tileSize) {
 		disallowedDirections.push("top");
-	} else if (lastTile.topLeft[0] >= 120) {
+	} else if (lastTile.topLeft[0] >= tileSize*3) {
 		disallowedDirections.push("right");
-	} else if (lastTile.topLeft[1] >= 120) {
+	} else if (lastTile.topLeft[1] >= tileSize*3) {
 		disallowedDirections.push("bottom");
 	}
 
@@ -28,13 +28,13 @@ function generateTile(lastTile: Tile | null = null): [tileType, Coordinate] {
 	let cords: Coordinate = [lastTile.topLeft[0], lastTile.topLeft[1]];
 
 	if (from == "left") {
-		cords[0] += 40;
+		cords[0] += tileSize;
 	} else if (from == "right") {
-		cords[0] -= 40;
+		cords[0] -= tileSize;
 	} else if (from == "bottom") {
-		cords[1] -= 40;
+		cords[1] -= tileSize;
 	} else if (from == "top") {
-		cords[1] += 40;
+		cords[1] += tileSize;
 	}
 
 	return [type, cords];
@@ -54,7 +54,7 @@ export class Simulation {
 		this.tiles.push(this.roadGen.createTile({from: "top", to: "right"}, [0, 0]))
 
 		setInterval(() => {
-			this.tiles.push(this.roadGen.createTile(...generateTile(this.tiles[this.tiles.length - 1])))
+			this.tiles.push(this.roadGen.createTile(...generateTile(this.tiles[this.tiles.length - 1], this.tileSize)))
 
 			if  (this.tiles.length > 3) {
 				this.tiles.shift();
