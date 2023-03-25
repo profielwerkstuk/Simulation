6c2a812600cfeefb449d7d061edb171e1252fd33
Mardax <62964422+mardax007@users.noreply.github.com>
2023-03-25 17:46:18 +0100

    fix: :bug: Fixed validation

--- src/Mathamphetamine.ts
+++ src/Mathamphetamine.ts
@@ -17,15 +17,41 @@ export function getLineFormula(A: Coordinate, B: Coordinate): Line {
 	}
 }
 
+// Check if the intersection point is within the range of both lines
+function isWithinRange(line: Line, x: number, y: number): boolean {
+	const [xMin, xMax] = [Math.min(line.startingPoint[0], line.endingPoint[0]), Math.max(line.startingPoint[0], line.endingPoint[0])];
+	const [yMin, yMax] = [Math.min(line.startingPoint[1], line.endingPoint[1]), Math.max(line.startingPoint[1], line.endingPoint[1])];
+	return x >= xMin && x <= xMax && y >= yMin && y <= yMax;
+};
+
 export function getIntersect(lineA: Line, lineB: Line): Coordinate | null {
-	// x = (b - d) / (c - a)
+	// If a line is vertical
+	if (lineA.startingPoint[0] === lineA.endingPoint[0] || lineB.startingPoint[0] === lineB.endingPoint[0]) {
+		const line1 = (lineA.startingPoint[0] === lineA.endingPoint[0] ? lineA : lineB)
+		const line2 = (line1 == lineA ? lineB : lineA)
 
-	// Parallel lines
-	if (lineB.slope === lineA.slope) return null;
+		// calculate the x and y of the intersection point
+		const x = line1.startingPoint[0];
+		const y = line2.slope * x + line2.constant;
 
-	const xCoord = (lineA.constant - lineB.constant) / (lineB.slope - lineA.slope);
-	const yCoord = lineA.slope * xCoord + lineA.constant;
-	return [xCoord, yCoord];
+		// calculate the range of the line
+		if (!isWithinRange(lineA, x, y) || !isWithinRange(lineB, x, y)) {
+			return null;
+		}
+
+		return [x, y];
+	} else {
+		// calculate the x and y of the intersection point
+		const x = (lineB.constant - lineA.constant) / (lineA.slope - lineB.slope);
+		const y = lineA.slope * x + lineA.constant;
+
+		// calculate the range of the line
+		if (!isWithinRange(lineA, x, y) || !isWithinRange(lineB, x, y)) {
+			return null;
+		}
+
+		return [x, y];
+	}
 }
 
 export function distanceToLine(point: Coordinate, line: Line) {
