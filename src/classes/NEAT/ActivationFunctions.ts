901dfce90461bbb5c134bfca9e5f108ff980db4c
Maurits Wilke <mauritswilke@gmail.com>
2023-04-06 14:57:05 +0200

    feat: :sparkles: added more activation functions

--- src/classes/NEAT/ActivationFunctions.ts
+++ src/classes/NEAT/ActivationFunctions.ts
@@ -4,9 +4,11 @@ const SIGMOID: ActivationFunction = (input) => 1 / (1 + Math.exp(-input));
 
 const TANH: ActivationFunction = (input) => Math.tanh(input);
 
-const RELU: ActivationFunction = (input) => input > 0 ? input : 0;
+const RELU: ActivationFunction = (input) => Math.max(0, input);
 
-const Gaussian: ActivationFunction = (input) => Math.exp(-(input ** 2));
+const LRELU: ActivationFunction = (input) => Math.max(0.1 * input, input);
+
+const GAUSSIAN: ActivationFunction = (input) => Math.exp(-(input ** 2));
 
 const ELU: ActivationFunction = (input, alpha) => ((input > 0) ? input : (alpha! * Math.expm1(input)));
 
@@ -14,6 +16,21 @@ const SELU: ActivationFunction = (input) => 1.0507 * ELU(input, 1.67326);
 
 const STEP: ActivationFunction = (input) => input > 0 ? 1 : 0;
 
-const ActivationFunctions = { SIGMOID: SIGMOID, TANH: TANH, RELU: RELU, Gaussian: Gaussian, ELU: ELU, SELU: SELU, STEP: STEP };
+const LINEAR: ActivationFunction = (input) => input;
+
+const SWISH: ActivationFunction = (input) => input / (1 + Math.exp(-input));
+
+const ActivationFunctions = {
+	SIGMOID: SIGMOID,
+	TANH: TANH,
+	LRELU: LRELU,
+	RELU: RELU,
+	GAUSSIAN: GAUSSIAN,
+	ELU: ELU,
+	SELU: SELU,
+	STEP: STEP,
+	LINEAR: LINEAR,
+	SWISH: SWISH
+};
 
 export { ActivationFunctions };
